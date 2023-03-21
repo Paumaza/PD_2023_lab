a7d8f83baf61d9e6d8d1ca532a8cd3b15ae28cc3
Pau Maza <pau.maza@hotmail.com>
2023-03-21 20:20:20 +0100

    classe xinos

--- Practica5/projecte/src/main.cpp
+++ Practica5/projecte/src/main.cpp
@@ -1,9 +1,122 @@
-#include <Arduino.h>
+#include <Wire.h>
+#include <AHT10.h>
+#if defined(ESP8266)
+#include <ESP8266WiFi.h>
+#endif
 
-void setup() {
-  // put your setup code here, to run once:
+float ahtValue;                               //to store T/RH result
+
+AHT10 aht10(AHT10_ADDRESS_0X38, AHT10_SENSOR); //sensor address, sensor type
+
+
+
+/**************************************************************************/
+/*
+    setup()
+    Main setup
+*/
+/**************************************************************************/
+void setup()
+{
+  #if defined(ESP8266)
+  WiFi.persistent(false);  //disable saving wifi config into SDK flash area
+  WiFi.forceSleepBegin();  //disable AP & station by calling "WiFi.mode(WIFI_OFF)" & put modem to sleep
+  #endif
+
+  Serial.begin(115200);
+  Serial.println();
+  
+  while (aht10.begin() != true) //for ESP-01 use aht10.begin(0, 2);
+  {
+    Serial.println(F("AHT1x not connected or fail to load calibration coefficient")); //(F()) save string to flash & keeps dynamic memory free
+
+    delay(5000);
+  }
+
+  Serial.println(F("AHT10 OK"));
+
+  //Wire.setClock(400000); //experimental I2C speed! 400KHz, default 100KHz
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+
+/**************************************************************************/
+/*
+    loop()
+     Main loop
+*/
+/**************************************************************************/
+void loop()
+{
+  /* DEMO - 1, every temperature or humidity call will read 6-bytes over I2C, total 12-bytes */
+  Serial.println();
+  Serial.println(F("DEMO 1: read 12-bytes"));
+
+  ahtValue = aht10.readTemperature(); //read 6-bytes via I2C, takes 80 milliseconds
+
+  Serial.print(F("Temperature...: "));
+  
+  if (ahtValue != AHT10_ERROR) //AHTXX_ERROR = 255, library returns 255 if error occurs
+  {
+    Serial.print(ahtValue);
+    Serial.println(F(" +-0.3C"));
+  }
+  else
+  {
+    //printStatus(); //print temperature command status
+
+    if   (aht10.softReset() == true) Serial.println(F("reset success")); //as the last chance to make it alive
+    else                             Serial.println(F("reset failed"));
+  }
+
+  delay(2000); //measurement with high frequency leads to heating of the sensor, see NOTE
+
+  ahtValue = aht10.readHumidity(); //read another 6-bytes via I2C, takes 80 milliseconds
+
+  Serial.print(F("Humidity......: "));
+  
+  if (ahtValue != AHT10_ERROR) //AHTXX_ERROR = 255, library returns 255 if error occurs
+  {
+    Serial.print(ahtValue);
+    Serial.println(F(" +-2%"));
+  }
+  else
+  {
+    //printStatus(); //print humidity command status
+  }
+
+  delay(2000); //measurement with high frequency leads to heating of the sensor, see NOTE
+
+  /* DEMO - 2, temperature call will read 6-bytes via I2C, humidity will use same 6-bytes */
+  Serial.println();
+  Serial.println(F("DEMO 2: read 6-byte"));
+
+  ahtValue = aht10.readTemperature(); //read 6-bytes via I2C, takes 80 milliseconds
+
+  Serial.print(F("Temperature: "));
+  
+  if (ahtValue != AHT10_ERROR) //AHTXX_ERROR = 255, library returns 255 if error occurs
+  {
+    Serial.print(ahtValue);
+    Serial.println(F(" +-0.3C"));
+  }
+  else
+  {
+    //printStatus(); //print temperature command status
+  }
+
+  ahtValue = aht10.readHumidity(AHT10_USE_READ_DATA); //use 6-bytes from temperature reading, takes zero milliseconds!!!
+
+  Serial.print(F("Humidity...: "));
+  
+  if (ahtValue != AHT10_ERROR) //AHTXX_ERROR = 255, library returns 255 if error occurs
+  {
+    Serial.print(ahtValue);
+    Serial.println(F(" +-2%"));
+  }
+  else
+  {
+    //printStatus(); //print temperature command status not humidity!!! RH measurement use same 6-bytes from T measurement
+  }
+
+  delay(10000); //recomended polling frequency 8sec..30sec
 }
