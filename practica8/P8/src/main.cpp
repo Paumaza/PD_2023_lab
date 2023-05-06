6cbdf7d597cb10cd14737766533aede9c149a254
Pau Maza <pau.maza@hotmail.com>
2023-05-06 10:03:04 +0200

    p8_ex2

--- practica8/P8/src/main.cpp
+++ practica8/P8/src/main.cpp
@@ -1,23 +1,52 @@
-#include <Arduino.h>
+#include <SoftwareSerial.h>
+#include <TinyGPS.h>
 
-void setup() {
-  // UART0 terminal
-  Serial.begin(115200);
+TinyGPS gps;
+SoftwareSerial softSerial(4, 3);
 
-  // UART2 periferic
-  Serial2.begin(115200);
+void setup()
+{
+   Serial.begin(115200);
+   softSerial.begin(9600);
 }
 
-void loop() {
-  // llegir  UART0 i escriure a UART2
-  if (Serial.available()) {
-    byte recibido = Serial.read();
-    Serial2.write(recibido);
-  }
+void loop()
+{
+   bool newData = false;
+   unsigned long chars;
+   unsigned short sentences, failed;
+   
+   // Intentar recibir secuencia durante un segundo
+   for (unsigned long start = millis(); millis() - start < 1000;)
+   {
+      while (softSerial.available())
+      {
+         char c = softSerial.read();
+         if (gps.encode(c)) // Nueva secuencia recibida
+            newData = true;
+      }
+   }
 
-  // llegir UART2 i escriure a UART0
-  if (Serial2.available()) {
-    byte recibido = Serial2.read();
-    Serial.write(recibido);
-  }
+   if (newData)
+   {
+      float flat, flon;
+      unsigned long age;
+      gps.f_get_position(&flat, &flon, &age);
+      Serial.print("LAT=");
+      Serial.print(flat == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flat, 6);
+      Serial.print(" LON=");
+      Serial.print(flon == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flon, 6);
+      Serial.print(" SAT=");
+      Serial.print(gps.satellites() == TinyGPS::GPS_INVALID_SATELLITES ? 0 : gps.satellites());
+      Serial.print(" PREC=");
+      Serial.print(gps.hdop() == TinyGPS::GPS_INVALID_HDOP ? 0 : gps.hdop());
+   }
+
+   gps.stats(&chars, &sentences, &failed);
+   Serial.print(" CHARS=");
+   Serial.print(chars);
+   Serial.print(" SENTENCES=");
+   Serial.print(sentences);
+   Serial.print(" CSUM ERR=");
+   Serial.println(failed);
 }
