01936a5b4235c8f420d4dd111400aa4110dcc7de
Pau Maza <pau.maza@hotmail.com>
2023-05-06 09:32:37 +0200

    p8_ex1

--- practica8/P8/src/main.cpp
+++ practica8/P8/src/main.cpp
@@ -1,9 +1,23 @@
 #include <Arduino.h>
 
 void setup() {
-  // put your setup code here, to run once:
+  // UART0 terminal
+  Serial.begin(115200);
+
+  // UART2 periferic
+  Serial2.begin(115200);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // llegir  UART0 i escriure a UART2
+  if (Serial.available()) {
+    byte recibido = Serial.read();
+    Serial2.write(recibido);
+  }
+
+  // llegir UART2 i escriure a UART0
+  if (Serial2.available()) {
+    byte recibido = Serial2.read();
+    Serial.write(recibido);
+  }
 }
