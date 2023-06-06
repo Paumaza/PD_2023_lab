1beabb1a59e370ee6cdadeb7be9b9460c05fc189
Pau Maza <pau.maza@hotmail.com>
2023-06-06 09:25:45 +0200

    versio final, no escolto audio

--- Final_Project/Theremin/src/main.cpp
+++ Final_Project/Theremin/src/main.cpp
@@ -36,19 +36,10 @@ const int bps = 16;
 
 const int halfWavelength = (sampleRate / frequency); // half wavelength of square wave
 
-short sample = amplitude; // current sample value
+int sample = amplitude; // current sample value
 int count = 0;
 
-i2s_mode_t mode = I2S_PHILIPS_MODE; // I2S decoder is needed
-// i2s_mode_t mode = ADC_DAC_MODE; // Audio amplifier is needed
-
-// Mono channel input
-// This is ESP specific implementation -
-//   samples will be automatically copied to both channels inside I2S driver
-//   If you want to have true mono output use I2S_PHILIPS_MODE and interlay
-//   second channel with 0-value samples.
-//   The order of channels is RIGH followed by LEFT
-//i2s_mode_t mode = I2S_RIGHT_JUSTIFIED_MODE; // I2S decoder is needed
+i2s_mode_t mode = ADC_DAC_MODE; // Audio amplifier is needed
 
 void setup() {
   Serial.begin(115200);
@@ -58,40 +49,47 @@ void setup() {
     while (1); // do nothing
   }
 
-  pinMode(SPEAKER_PIN, OUTPUT);
-  pinMode(LRC_PIN, OUTPUT);
-  pinMode(BCLK_PIN, OUTPUT);
+  // pinMode(SPEAKER_PIN, OUTPUT);
+  // pinMode(LRC_PIN, OUTPUT);
+  // pinMode(BCLK_PIN, OUTPUT);
 
-  // Initialize ToneAC library
-  // ToneAC.begin(SPEAKER_PIN);
+  I2S.setFsPin(LRC_PIN);
+  I2S.setDataPin(SPEAKER_PIN);
+  I2S.setSckPin(BCLK_PIN);
 }
 
 void loop() {
   float freqDistance = ultrasonicFreq.read(); // 5.80; // Convert the echo time to distance in centimeters
   float ampDistance = ultrasonicAmp.read(); // 5.80;   // Convert the echo time to distance in centimeters
-  Serial.print("read ultrasonic sensor ok");
 
   // Map the distance values to the desired range for frequency and amplitude
   int freq = map(freqDistance, 0, FREQ_MAX_DISTANCE, 100, 1000);
-  int amp = map(ampDistance, 0, AMP_MAX_DISTANCE, 0, 255);
   Serial.print("Distance 1: ");
   Serial.println(freqDistance);
   Serial.print("Distance 2: ");
   Serial.println(ampDistance);
+
+  int amp = map(ampDistance, 0, AMP_MAX_DISTANCE, 0, 255);
+
   Serial.print("Frequency: ");
   Serial.println(freq);
   Serial.print("Amplitude: ");
   Serial.println(amp);
 
+  sample = amp * sin(2.0 * PI * freq * count / sampleRate);
+  Serial.print("Sample: ");
+  Serial.println(sample);
+
   // Generate the audio tone with the calculated frequency and amplitude
-  if (count % halfWavelength == 0 ) {
-      // invert the sample every half wavelength count multiple to generate square wave
-      sample = -1 * sample;
-    }
+  // if (count % halfWavelength == 0 ) {
+  //     // invert the sample every half wavelength count multiple to generate square wave
+  //     sample = -1 * sample;
+  //   }
 
   if(mode == I2S_PHILIPS_MODE || mode == ADC_DAC_MODE){ // write the same sample twice, once for Right and once for Left channel
     I2S.write(sample); // Right channel
     I2S.write(sample); // Left channel
+    Serial.println("IN LR");
     }
   else if(mode == I2S_RIGHT_JUSTIFIED_MODE || mode == I2S_LEFT_JUSTIFIED_MODE){
     // write the same only once - it will be automatically copied to the other channel
