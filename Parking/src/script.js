df6ddb2b09ecf7884611090deea2adb6d1e41140
Pau Maza <pau.maza@hotmail.com>
2023-06-21 22:01:29 +0200

    improoved javascript and html

--- Parking/src/script.js
+++ Parking/src/script.js
@@ -4,6 +4,25 @@ document.addEventListener("DOMContentLoaded", function() {
     const calculateButton = document.getElementById("calculate-button");
     const bestFitContainer = document.getElementById("best-fit");
     const parkingSpot = document.getElementById("parking-spot");
+
+    const parking = {
+      floor: [
+        { spot: [{ length: 20, width: 40 }, { length: 24, width: 45 }, { length: 28, width: 50 }] },
+        { spot: [{ length: 40, width: 60 }, { length: 44, width: 65 }, { length: 48, width: 69 }] },
+        { spot: [{ length: 60, width: 70 }, { length: 64, width: 75 }, { length: 100, width: 800 }] }
+      ]
+    };
+
+    function bestSpot(parking, carLength, carWidth) {
+      for (let floor = 0; floor < 3; floor++) {
+        for (let spot = 0; spot < 3; spot++) {
+          if (carLength <= parking.floor[floor].spot[spot].length && carWidth <= parking.floor[floor].spot[spot].width) {
+            return [floor, spot];
+          }
+        }
+      }
+      return [-1, -1];
+    }
   
     calculateButton.addEventListener("click", function() {
       const carWidth = parseInt(carWidthInput.value);
@@ -13,9 +32,12 @@ document.addEventListener("DOMContentLoaded", function() {
       // You can use fetch() or XMLHttpRequest to send the request
   
       // Example response data (replace this with the actual response from the server)
+
+      const result = bestSpot(parking, carLength, carWidth);
+
       const responseData = {
-        floor: 2,
-        spot: 3
+        floor: result[0],
+        spot: result[1]
       };
   
       // Display the best fit result
